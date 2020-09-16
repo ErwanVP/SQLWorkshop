88428bd6233bb4afeb2d61323c6742c10336a086
Erwan van Peteghem <erwan.vanpeteghem@ricardo.ch>
2020-09-16 18:05:23 +0200

    add change

--- Day1/Demo/020-psql-consistency.sql
+++ Day1/Demo/020-psql-consistency.sql
@@ -4,7 +4,7 @@ SELECT * FROM trans;
 BEGIN TRANSACTION;
     INSERT INTO trans SELECT 3 , 'Trans 3';
 
-    INSERT INTO trans SELECT 4 , 'Trans 4';
+    INSERT INTO trans SELECT 5 , 'Trans 5';
 
 COMMIT;
 
