08cde5230b08530e1f8c5087a846bbb1b7f3e78a
Erwan van Peteghem <erwan.vanpeteghem@ricardo.ch>
2020-09-16 17:56:57 +0200

    made some change

--- Day2/Postgres/02-missing-index.sql
+++ Day2/Postgres/02-missing-index.sql
@@ -1,6 +1,53 @@
-SELECT   relname, seq_scan, seq_tup_read, 
-      idx_scan, idx_tup_fetch, 
-      seq_tup_read / seq_scan 
-  FROM   pg_stat_user_tables 
-  WHERE   seq_scan > 0 
-  ORDER BY seq_tup_read DESC;
+SELECT
+  relname,
+  seq_scan - idx_scan AS too_much_seq,
+  CASE
+    WHEN seq_scan - coalesce(idx_scan, 0) > 0 THEN 'Missing Index ?'
+    ELSE 'OK'
+  END,
+  pg_relation_size(relname::regclass) AS rel_size, 
+  seq_scan, idx_scan
+FROM pg_stat_all_tables
+WHERE schemaname = 'public' AND pg_relation_size(relname::regclass) > 80000 
+ORDER BY too_much_seq DESC;
+
+SELECT
+  x1.table_in_trouble,
+  pg_relation_size(x1.table_in_trouble) AS sz_n_byts,
+  x1.seq_scan,
+  x1.idx_scan,
+  CASE
+  WHEN pg_relation_size(x1.table_in_trouble) > 500000000
+    THEN 'Exceeds 500 megs, too large to count in a view. For a count, count individually'::text
+  ELSE count(x1.table_in_trouble)::text
+  END                                   AS tbl_rec_count,
+  x1.priority
+FROM
+  (
+    SELECT
+      (schemaname::text || '.'::text) || relname::text AS table_in_trouble,
+      seq_scan,
+      idx_scan,
+      CASE
+      WHEN (seq_scan - idx_scan) < 500
+        THEN 'Minor Problem'::text
+      WHEN (seq_scan - idx_scan) >= 500 AND (seq_scan - idx_scan) < 2500
+        THEN 'Major Problem'::text
+      WHEN (seq_scan - idx_scan) >= 2500
+        THEN 'Extreme Problem'::text
+      ELSE NULL::text
+      END AS priority
+    FROM
+      pg_stat_all_tables
+    WHERE
+      seq_scan > idx_scan
+      AND schemaname != 'pg_catalog'::name
+    AND seq_scan > 100) x1
+GROUP BY
+  x1.table_in_trouble,
+  x1.seq_scan,
+  x1.idx_scan,
+  x1.priority
+ORDER BY
+  x1.priority DESC,
+  x1.seq_scan;
--- Day2/exercices/02-explains-queries.sql
+++ Day2/exercices/02-explains-queries.sql
@@ -32,16 +32,53 @@ INNER JOIN cart_items ci ON c.id = ci.cart_id
 WHERE 
 c.id in ('236227','2362230');
 
--- query 4 Get carts with all items link by cart.name
+-- query 4 Get carts with all items link by cart.name 
 EXPLAIN (ANALYZE, COSTS, VERBOSE, BUFFERS, FORMAT JSON)
-SELECT cart_id, 
-cart_item, 
-seller_id, 
-article_id, 
-article_name, 
-price, 
-ci.created_at, 
-ci.updated_at FROM carts c
-INNER JOIN cart_items ci ON c.id = ci.cart_id 
+SELECT id, 
+user_id,
+name, 
+created_at, 
+updated_at
+FROM carts c
+WHERE 
+name = 'name_407819_407819_407819_407819_4078' ;
+
+-- Create index
+CREATE INDEX CONCURRENTLY carts_name ON carts (
+name)
+WITH (FILLFACTOR = 90) 
+
+-- do again the select
+EXPLAIN (ANALYZE, COSTS, VERBOSE, BUFFERS, FORMAT JSON)
+SELECT id, 
+user_id,
+name, 
+created_at, 
+updated_at
+FROM carts c
+WHERE 
+name = 'name_407819_407819_407819_407819_4078' ;
+
+-- do again the select
+EXPLAIN (ANALYZE, COSTS, VERBOSE, BUFFERS, FORMAT JSON)
+SELECT
+name
+FROM carts c
+WHERE 
+name = 'name_407819_407819_407819_407819_4078' ;
+
+-- But I need the id
+
+DROP INDEX CONCURRENTLY carts_name;
+
+CREATE INDEX CONCURRENTLY carts_name ON carts (
+name) INCLUDE (id)
+WITH (FILLFACTOR = 90) 
+
+-- do again the select with id
+EXPLAIN (ANALYZE, COSTS, VERBOSE, BUFFERS, FORMAT JSON)
+SELECT id,
+name
+FROM carts c
 WHERE 
 name = 'name_407819_407819_407819_407819_4078' ;
